5e2b37efbc4b9c1b19edec02d09f15b040967893
bobur <boburotaboyev0@gmail.com>
2024-04-20 09:58:18 +0500

    20.4.2024 (1)

---
+++ modul11_2_1/lib/logic/services/weather_api_services.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:modul11_2_1/data/constants/constants.dart';
+import 'package:modul11_2_1/data/models/weather.dart';
+import 'package:modul11_2_1/logic/services/weather_exception.dart';
+
+class WeatherAPIService {
+  final http.Client client;
+
+  WeatherAPIService({required this.client});
+
+  Future<Weather> getWeather(String city) async {
+    final url = Uri.parse("$base_url?q=${city.toLowerCase()}&appid=$api_key");
+
+    try {
+      final response = await client.get(url);
+      if (response.statusCode >= 400) {
+        throw WeatherExceptions(response.reasonPhrase.toString());
+      }
+      final responseBody = jsonDecode(response.body);
+
+      if (responseBody == null) {
+        throw WeatherExceptions('Cannot get weather for $city');
+      }
+      final data = responseBody as Map<String, dynamic>;
+
+      final weatherData = data['weather'][0];
+      final mainData = data['main'];
+
+      final Weather weather = Weather(
+        id: weatherData['id'],
+        main: weatherData['main'],
+        desc: weatherData['description'],
+        icon: weatherData['icon'],
+        temp: double.parse(mainData['temp'].toString()),
+        city: city,
+      );
+      return weather;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
---
+++ modul11_2_1/lib/logic/services/weather_exception.dart
@@ -0,0 +1,10 @@
+class WeatherExceptions implements Exception {
+  final String message;
+
+  WeatherExceptions(this.message);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
